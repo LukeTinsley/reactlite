9ede7735addf0fee385e6d0e00015b1a3d4a6571
Luke Tinsley <luketinsley@LMCIA.local>
2023-01-12 00:14:20 -0500

    first successful run, starting point for redux implementation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="heading">React Boilerplate Template</h1>
+            <h4 className="sub-heading">
+                This is a barebones react app template.
+            </h4>
+        </div>
+    );
+};
+
+export default App;
